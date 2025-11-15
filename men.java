984d0f5d865b785a5033dcf69c2cebf6efcbb0c9
maheshkohli <maheshkohlircb@gmail.com>
2025-11-15 11:02:24 +0530

    Create men.java

---
+++ men.java
@@ -0,0 +1,8 @@
+public class Sample
+{
+   public static void main(String[] args)
+   {
+      // Demonstrate println
+      System.out.println("This string is displayed on one line.");
+      System.out.println("This string appears on the second line.");
+      System.out.println(); // The third line displayed is blank
